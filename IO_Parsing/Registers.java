da05764a1380d8bee84f408db29b37c56e0f2ba2
Earvin Leonardo <ivleonardo@up.edu.ph>
2017-05-06 13:48:41 +0800

    added flag registers

--- IO_Parsing/Registers.java
+++ IO_Parsing/Registers.java
@@ -9,5 +9,11 @@ public class Registers extends LinkedHashMap<String,Integer> {
 		for(int i=1; i<=32; i+=1) {
 			this.put(R+Integer.toString(i),0);
 		}
+		this.put("PC", 0);		//PROGRAM COUNTER
+		this.put("MAR", 0);		//ADDRESS TO BE FETCHED
+		this.put("MBR", 0);		//VALUE OF FETCHED ADDRESS
+		this.put("OF", 0);		//TRIGGERS WHEN THERE IS OVERFLOW
+		this.put("NF", 0);		//TRIGGERS FOR NEGATIVE INTEGERS
+		this.put("ZF", 0);		//FOR COMPARING TO INTEGERS
 	}
 }
