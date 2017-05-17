530d74333122514959cae6c3df1bcdb5583f28fd
rcescoto <rcescoto@up.edu.ph>
2017-05-18 01:50:51 +0800

    Updated amrs

--- AMRS.java
+++ AMRS.java
@@ -9,9 +9,21 @@ class AMRS {
 		ArrayList<Instruction> ins = new ArrayList<Instruction>();
 		int clock_cycle=0;
 		boolean notDone=true;
-
+		Fetch f = new Fetch();
+		Decode d = new Decode();
+		Execute e = new Execute();
+		Memory m = new Memory();
+		Write w = new Write();
 		for(int i=0; i<io.instructions.size(); i++){
 			ins.add(new Instruction(i+1));
+		}
+		while(1){//master loop
+			if(clock_cycle == 0){
+
+			}
+			else{
+
+			}
 		}
 	}
 }
