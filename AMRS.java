f4c172b1eccd525f466dba9da40ea8a8cc4728eb
Earvin Leonardo <ivleonardo@up.edu.ph>
2017-05-18 04:13:31 +0800

    modified for error checking

--- AMRS.java
+++ AMRS.java
@@ -1,11 +1,10 @@
 import java.util.*;
 import IO_Parsing.*;
-import Stages.*;
+//import Stages.*;
 class AMRS {
 	public static void main(String args[]) {
-		IO io = new IO("instructions.txt");
 		Registers r = new Registers();
-		//Parser p = new Parser(io.instructions,r);
+		IO io = new IO("instructions.txt", r);
 		ArrayList<Instruction> ins = new ArrayList<Instruction>();
 		int clock_cycle=0;
 		boolean notDone=true;
@@ -14,7 +13,7 @@ class AMRS {
 		Execute e = new Execute();
 		Memory m = new Memory();
 		Write w = new Write();
-		Hazard h = new Hazard();
+		Hazard h = new Hazard();*/
 		for(int i=0; i<io.instructions.size(); i++){
 			ins.add(new Instruction(i+1));
 		}
--- IO_Parsing/IO.java
+++ IO_Parsing/IO.java
@@ -10,7 +10,9 @@ import java.util.*;
 public class IO {
 	public ArrayList<ArrayList<String>> instructions = new ArrayList<ArrayList<String>>();
 	public ArrayList<String> temp;
-	public IO(String filename) {
+	public ArrayList<String> library = new ArrayList<String>();
+	public boolean valid = true;
+	public IO(String filename, Registers r) {
 		try {														
 			BufferedReader reader = new BufferedReader(new FileReader(filename));
 			String line;
@@ -26,5 +28,58 @@ public class IO {
 				this.instructions.add(temp);
 			}
 		} catch(IOException e) {}
+		
+		this.library.add("LOAD");
+		this.library.add("ADD");
+		this.library.add("SUB");
+		this.library.add("CMP");
+		
+		for (int i=0; i<instructions.size(); i+=1) {
+			for(int j=0; j<instructions.get(i).size(); j+=1) {
+			}	
+			String inst = instructions.get(i).get(0);
+			String reg = instructions.get(i).get(1);
+			if(!this.library.contains(instructions.get(i).get(0))) {// reads the first element in the arraylist of the current instruction
+				valid = false;
+				System.out.println("Syntax Error! Instruction does not exist.");
+				break;
+			}
+			if(!r.containsKey(instructions.get(i).get(1))) {
+				valid = false;
+				System.out.println("Syntax Error! Register does not exist.");
+				break;
+			}
+			if(inst.matches("LOAD")) {
+				if(!instructions.get(i).get(2).matches("^-?[0-9]|[1-9][0-9]")) {
+					valid = false;
+					System.out.println("Syntax Error!");
+					break;
+				}
+			}
+			else if(inst.matches("ADD")) {
+				if(r.containsKey(reg) && r.containsKey(instructions.get(i).get(2))) {
+				}
+				else {
+					System.out.println("Syntax Error!");
+					break;
+				}
+			}
+			else if(inst.matches("SUB")) {
+				if(r.containsKey(reg) && r.containsKey(instructions.get(i).get(2))) {
+				}
+				else {
+					System.out.println ("Syntax Error!");
+					break;
+				}
+			}
+			else if(inst.matches("CMP")) {
+				if(r.containsKey(reg) && r.containsKey(instructions.get(i).get(2))) {
+				}
+				else {
+					System.out.println("Syntax Error!");
+					break;
+				}			
+			}
+		}
 	}
 }
